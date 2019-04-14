36e163f4afd8c150f6217f6fab32c5af4b67ec6b
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-04-14 14:04:40 -0500

    added box functionality for messages

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ let user = rltm({
 service: 'pubnub',
 config: {
     publishKey: 'pub-c-0dda1bca-3013-459f-8333-32b487e74ab4',
-    subscribeKey: 'sub-c-f43f4c62-5c6c-11e9-af7f-e675e2b0822b'
-      }
+    subscribeKey: 'sub-c-f43f4c62-5c6c-11e9-af7f-e675e2b0822b',
+    uuid: 'test_user'
+  }
 });
 
 room = user.join('trip-check');
@@ -12,7 +13,7 @@ function submit() {
   var boxVal = document.getElementById("dmBox").value;
 
   if(boxVal != "") {
-    room.message({hello: boxVal});
+    room.message({message: boxVal});
     $('#dmBox').val("");
   }
 }
@@ -22,12 +23,38 @@ function submit() {
 // });
 
 room.on('message', (uuid, data) => {
-  alert(data.hello);
+  if(uuid == 'test_user'){
+    createBoxForCurrUser(data);
+  }
+  else{
+    createBoxForOtherUser(data);
+  }
 });
 
 // room.history().then((history) => {
 //   console.log('got array of all messages in channel', history);
 // });
+function createBoxForCurrUser(data) {
+  var box = document.createElement('div');
+  var dm = document.getElementById("dm");
+
+  dm.appendChild(document.createElement('br'));
+
+  box.style.width = "27vw";
+  box.style.height = "10vh";
+
+  box.style.textAlign = "center";
+  box.style.background = "white";
+
+  box.style.position = "relative";
+  box.style.top = "100px;"
+
+  box.style.borderRadius = "10px";
+
+  box.innerHTML = data.message;
+
+  dm.appendChild(box);
+}
 
 $(document).keypress(function(e) {
     if(e.which == 13) {
