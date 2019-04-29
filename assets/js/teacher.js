d731fbb8a523635a6b720fc8b24ea3ea97c898c2
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-04-29 18:29:44 -0500

    added log in text

--- assets/js/teacher.js
+++ assets/js/teacher.js
@@ -1,8 +1,10 @@
+
 // Google Sign-In
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
+  document.write('<p id="idDiv"> logged in as: ' + profile.getName() + '</p>');
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
