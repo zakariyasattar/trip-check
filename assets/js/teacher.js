a4dccfc3f2a8bdff2188145c39661ffbe44f1883
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-04-30 08:40:47 -0500

    add auth2 var

--- assets/js/teacher.js
+++ assets/js/teacher.js
@@ -12,6 +12,7 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     window.location = "/index.html";
   });
