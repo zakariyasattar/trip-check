6b42a6dd28e4eeac8e915893a5098eee56e364a7
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-04-29 19:19:03 -0500

    add signOut button

--- assets/js/teacher.js
+++ assets/js/teacher.js
@@ -10,7 +10,7 @@ function onSignIn(googleUser) {
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    console.log('User signed out.');
+    window.location = "/index.html";
   });
 }
 
