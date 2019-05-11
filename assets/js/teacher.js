33ee61cbefdd1515347a76e768a43a5097a8b223
Zakariya Sattar <zakariyasattar03@gmail.com>
2019-05-10 20:46:42 -0500

    add signOut code when page load

--- assets/js/teacher.js
+++ assets/js/teacher.js
@@ -6,13 +6,6 @@ function onSignIn(googleUser) {
   localStorage.setItem("userInfo", JSON.stringify([profile.getId(), profile.getName(), profile.getImageUrl(), profile.getEmail()]));
 }
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    alert("signed out");
-  });
-}
-
 function clickSignOut() {
   window.location = "../../index.html";
 }
